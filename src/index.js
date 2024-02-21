b07c663febb48aa23d8c4cdb3969658e38cc54a2
Darknab <117382045+Darknab@users.noreply.github.com>
2024-02-21 09:06:41 +0100

    Test the configuration.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import { Todo } from "./todo";
+// import { Project } from "./project";
+
+const test = document.createElement('h1');
+
+test.textContent = 'Now we are talking!';
+
+document.body.prepend(test);
+
+console.log('hello!!!');
+
+
+
