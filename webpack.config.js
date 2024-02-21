8c8ff58f38560e6eb9b982d03ea07559a53afc65
Darknab <117382045+Darknab@users.noreply.github.com>
2024-02-21 09:06:10 +0100

    Install html-webpack-plugin and webpack-dev-server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,6 +7,20 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Todo app',
+    }),
+ ],
+  devtool: 'inline-source-map',
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    }
   },
   module: {
     rules: [
