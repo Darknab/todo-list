eaed54c62abfbe088a0c7eb525867319ca808d2d
Darknab <117382045+Darknab@users.noreply.github.com>
2024-02-22 08:53:40 +0100

    Create a template for static content.

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [new HtmlWebpackPlugin({
-    title: 'Todo app',
+    template: './src/template.html',
     }),
  ],
   devtool: 'inline-source-map',
