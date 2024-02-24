d90554d139791be2fa64e654c2be6c0a23a4ec73
Darknab <117382045+Darknab@users.noreply.github.com>
2024-02-24 19:40:12 +0100

    Add a button to show/hide the task description

--- src/content.js
+++ src/content.js
@@ -5,27 +5,44 @@ const tasks = document.querySelector('.tasks');
 function addElement(task) {
   const element = document.createElement('div');
   element.classList.add('task-element');
+  const firstLine = document.createElement('div');
+  firstLine.classList.add('first-line');
   const taskTitle = document.createElement('span');
+  taskTitle.classList.add('task-title');
   taskTitle.textContent = task.title;
   const taskDueDate = document.createElement('span');
+  taskDueDate.classList.add('task-date');
+  const more = document.createElement('button');
+  more.classList.add('more');
+  more.textContent = 'Show more';
+  more.role = 'link';
   taskDueDate.textContent = task.dueDate;
   const taskDescription = document.createElement('p');
   taskDescription.classList.add('task-description', 'closed');
   taskDescription.textContent = task.description;
 
-  element.append(taskTitle, taskDueDate, taskDescription);
+  firstLine.append(taskTitle, taskDueDate, more)
+  element.append(firstLine, taskDescription);
   tasks.append(element);
 }
 
-function activateAccordeon() {
+function toggleButtonText(target) {
+  if (target.textContent === 'Show more') {
+    target.textContent = 'Show less';
+  } else target.textContent = 'Show more';
+}
+
+function showMore() {
   document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('task-element')) {
-      const description = e.target.querySelector('.task-description');
+    if (e.target.classList.contains('more')) {
+      //select the task-element div related to the button
+      const task = e.target.parentNode.parentNode;
+      const description = task.querySelector('.task-description');
       description.classList.toggle('closed');
+      toggleButtonText(e.target);
     }
   })
 }
-
 
 export function displayTasks() {
   document.addEventListener('click', (e) => {
@@ -45,5 +62,5 @@ export function displayTasks() {
       }
     }
   })
-  activateAccordeon();
+  showMore();
 }
