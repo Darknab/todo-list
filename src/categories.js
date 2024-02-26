42173cfcd30a6e3367c3f31ffdce2d2372634907
Darknab <117382045+Darknab@users.noreply.github.com>
2024-02-26 10:42:42 +0100

    Write a function that create or retrieve categories from localStorage

--- src/categories.js
+++ src/categories.js
@@ -1,5 +1,26 @@
-export const categories = {
-  'default': [],
-  'work': [],
-  'hobbies': [],
+// Create categories in localStorage
+function createCategories() {
+  const categories = {
+    'default': [],
+  }
+  localStorage.setItem('categories', JSON.stringify(categories));
+  return categories;
 }
+
+// Retrieve categories from localStorage
+function retieveCategories() {
+  const categories = localStorage.getItem('categories');
+
+  return JSON.parse(categories);
+}
+
+export function getCategories() {
+  if (!localStorage.getItem('categories')) {
+    return createCategories();
+  } else return retieveCategories();
+}
+
+
+
+
+
