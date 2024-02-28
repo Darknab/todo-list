0a5f969d6900b4ed512a1f80c84d42df45a07862
Darknab <117382045+Darknab@users.noreply.github.com>
2024-02-28 09:05:23 +0100

    Create a form in a modal to add and save new tasks

--- src/index.js
+++ src/index.js
@@ -4,40 +4,63 @@ import { Todo } from "./todo";
 import { displayCategories } from './nav';
 import { displayTasks } from './content';
 import { getCategories } from './categories';
+import { format } from 'date-fns';
 
 const categories = getCategories();
-
-const newNote = new Todo;
-
-newNote.title = 'New note';
-newNote.category = 'default';
-newNote.complete = false;
-newNote.description = 'A very important thing to do';
-newNote.dueDate = '21/02/2024';
-newNote.priority = 'Urgent';
-
-const task1 = new Todo('Task 1', 'blablabla', '28/02/2024', 'important', false, 'work');
-const task2 = new Todo('Task 2', 'blablabla', '26/02/2024', 'not important', false, 'work');
-const task3 = new Todo('Task 3', 'blablabla', '06/03/2024', 'important', true, 'work');
-const task4 = new Todo('Task 4', 'blablabla', '28/02/2024', 'dunno', false, 'hobbies');
-const task5 = new Todo('Task 5', 'blablabla', '28/02/2024', 'important', false, 'work');
-const task6 = new Todo('Task 6', 'blablabla', '28/02/2024', 'important', false, 'hobbies');
-
-console.log(task1)
-task1.updateCategory();
-task2.updateCategory();
-task3.updateCategory();
-task4.updateCategory();
-task5.updateCategory();
-task6.updateCategory();
-
-
-newNote.updateCategory();
-
-console.log(categories);
 
 displayCategories();
 displayTasks();
 
+const dialog = document.querySelector('dialog');
 
+const addTask = document.querySelector('#add-task');
+addTask.addEventListener('click', () => {
+  dialog.showModal();
+  populateSelect();
+})
+
+function clearElement(element) {
+  element.innerHTML = '';
+}
+
+function populateSelect() {
+  const select = document.querySelector('#select-category');
+  // Clear the select node to refresh the categories list
+  clearElement(select);
+  const cat = getCategories();
+  for (const catName in cat ) {
+    const option = document.createElement('option');
+    option.value = catName;
+    option.textContent = catName;
+    select.appendChild(option);
+  }
+}
+
+const submitTask = document.querySelector('#submit-task');
+submitTask.addEventListener('click', (e) => {
+  e.preventDefault();
+  const task = createTask();
+  saveTask(task);
+  displayCategories();
+  dialog.close();
+})
+
+function createTask() {
+  const task = new Todo;
+  task.title = document.querySelector('#title').value;
+  task.description = document.querySelector('#description').value;
+  task.category = document.querySelector('#select-category').value;
+  const date = document.querySelector('#due-date').value;
+  const formattedDate = format(new Date(date), 'dd/MM/yyyy');
+  task.date = formattedDate;
+  task.priority = document.querySelector('#priority');
+  task.complete = false;
+  return task;
+}
+
+function saveTask(task) {
+  categories[task.category].push(task);
+  const updatedCategories = JSON.stringify(categories);
+  localStorage.setItem('categories', updatedCategories);
+}
 
