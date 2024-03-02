e5681041ab8d6ff2e029861d964a8dd7046d4826
Darknab <117382045+Darknab@users.noreply.github.com>
2024-03-02 11:16:46 +0100

    Remove bundle.js from .gitignore

---
+++ bundle.js
@@ -0,0 +1,22 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// import { Todo } from "./todo";
+// import { Project } from "./project";
+
+const test = document.createElement('h1');
+
+test.textContent = 'working???';
+
+document.body.prepend(test);
+
+console.log('hello!!!');
+
+
+
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsWUFBWSxPQUFPO0FBQ25CLFlBQVksVUFBVTs7QUFFdEI7O0FBRUE7O0FBRUE7O0FBRUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLWxpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IHsgVG9kbyB9IGZyb20gXCIuL3RvZG9cIjtcbi8vIGltcG9ydCB7IFByb2plY3QgfSBmcm9tIFwiLi9wcm9qZWN0XCI7XG5cbmNvbnN0IHRlc3QgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdoMScpO1xuXG50ZXN0LnRleHRDb250ZW50ID0gJ3dvcmtpbmc/Pz8nO1xuXG5kb2N1bWVudC5ib2R5LnByZXBlbmQodGVzdCk7XG5cbmNvbnNvbGUubG9nKCdoZWxsbyEhIScpO1xuXG5cblxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
